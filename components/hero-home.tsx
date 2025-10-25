9ed85c745249e2e18a7689d6e60266dc3b7410f6
Rasheed Ayoade <rashotech@gmail.com>
2025-10-25 16:29:59 +0100

    fix: enhance animations and layout in HeroHome and ServiceCard components

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -19,6 +19,8 @@ export default function HeroHome() {
             <div
               className='mb-8'
               data-aos='fade-up'
+              data-aos-duration='600'
+              data-aos-once='true'
             >
               <div className='-mx-0.5 flex justify-center -space-x-3'>
                 {[Avatar01, Avatar02, Avatar03, Avatar04, Avatar05, Avatar06].map((avatar, index) => (
@@ -31,6 +33,8 @@ export default function HeroHome() {
                     alt={`Avatar ${index + 1}`}
                     data-aos='zoom-in'
                     data-aos-delay={100 + index * 50}
+                    data-aos-duration='400'
+                    data-aos-once='true'
                   />
                 ))}
               </div>
@@ -42,6 +46,8 @@ export default function HeroHome() {
                   className='block'
                   data-aos='fade-up'
                   data-aos-delay={150}
+                  data-aos-duration='600'
+                  data-aos-once='true'
                 >
                   Evolutionary development
                 </span>
@@ -49,6 +55,8 @@ export default function HeroHome() {
                   className='block text-blue-400'
                   data-aos='fade-up'
                   data-aos-delay={250}
+                  data-aos-duration='600'
+                  data-aos-once='true'
                 >
                   Incremental Delivery
                 </span>
@@ -57,6 +65,8 @@ export default function HeroHome() {
                 className='mx-auto max-w-2xl text-xl text-gray-300 leading-relaxed'
                 data-aos='fade-up'
                 data-aos-delay={300}
+                data-aos-duration='600'
+                data-aos-once='true'
               >
                 We help businesses adapt to the ever-changing digital landscape through evolutionary development and incremental delivery, ensuring continuous improvement and sustained success.
               </p>
